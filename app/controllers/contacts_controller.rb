10e5576b235b942f788d810ebce98a79f0877ed6
NachoVai <reyes.s.ign@gmail.com>
2022-01-04 20:52:30 -0300

    design contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,18 @@
 class ContactsController < ApplicationController
+
   def new
     @contact = Contact.new
   end
 
   def create
     @contact = Contact.create(name: params[:contact][:name], email: params[:contact][:email], message: params[:contact][:message])
-    render json: @contact
+    
+    if @contact.valid?
+      redirect_to contacts_new_path, alert: "*Mensaje enviado con éxito."
+    else
+      redirect_to contacts_new_path, alert: "*Ingrese un email válido."
+    end  
   end
+
+
 end
